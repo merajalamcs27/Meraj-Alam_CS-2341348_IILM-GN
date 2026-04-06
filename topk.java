d5f78a971389b229030e0698434229588057c592
merajalamcs27 <meraj.alam.cs27@iilm.edu>
2026-04-06 23:19:21 +0530

    Create topk.java

---
+++ topk.java
@@ -0,0 +1,30 @@
+// Top K Frequent Elements
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> freq = new HashMap<>();
+
+        for (int num : nums) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(
+            (a, b) -> freq.get(a) - freq.get(b)
+        );
+
+        for (int num : freq.keySet()) {
+            pq.offer(num);
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+
+        int[] result = new int[k];
+        int i = 0;
+
+        while (!pq.isEmpty()) {
+            result[i++] = pq.poll();
+        }
+
+        return result;
+    }
+}
