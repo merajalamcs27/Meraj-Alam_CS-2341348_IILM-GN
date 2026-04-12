e4b139ca2713ddb24ba7afea05f10ac282fc01ff
Meraj Alam <merajalamprivate@gmail.com>
2026-04-13 01:48:14 +0530

    Organize question files into DSA and resolve case-collision paths

--- dsa/arit.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(){
-    float a= 2;
-    float b = 5;
-    float ans = a/b;
-    printf("%f",ans);
-    return 0;
-}
--- dsa/pattern3.java
+++
@@ -1,3 +0,0 @@
-public class pattern3 {
-    static void main
-}
