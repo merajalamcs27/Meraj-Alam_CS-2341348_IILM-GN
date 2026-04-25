fa065c85904c7ca8b0200d28b26787191fa20b06
Meraj Alam <meraj.alam.cs27@iilm.edu>
2026-04-26 00:13:02 +0530

    0104-maximum-depth-of-binary-tree | 2026-04-26 00:13

--- DSA/0104-maximum-depth-of-binary-tree.java
+++ DSA/0104-maximum-depth-of-binary-tree.java
@@ -14,12 +14,12 @@
  * }
  */
 class Solution {
-    //faster version apply if condition before calling to reduce fn calls
     public int maxDepth(TreeNode root) {
-        if(root == null)return 0;
+        if(root == null )return 0;
         int left = maxDepth(root.left);
         int right = maxDepth(root.right);
         return Math.max(left,right)+1;
+
         
     }
 }
