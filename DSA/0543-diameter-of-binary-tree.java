c0998cf1a33f46ed3f7072f199d96d4eb49638e5
Meraj Alam <meraj.alam.cs27@iilm.edu>
2026-04-26 15:50:48 +0530

    0543-diameter-of-binary-tree | 2026-04-26 15:50

---
+++ DSA/0543-diameter-of-binary-tree.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int height(TreeNode root){
+        if(root == null)return 0;
+        return Math.max(height(root.left),height(root.right))+1;
+    }
+    public int diameterOfBinaryTree(TreeNode root) {
+        if(root == null)return 0;
+        int ld = (diameterOfBinaryTree(root.left));
+        int lh = (height(root.left));
+        int rd = (diameterOfBinaryTree(root.right));
+        int rh = (height(root.right));
+        int selfDiam = lh + rh ;
+        return Math.max(selfDiam,Math.max(ld,rd));
+        
+    }
+}
