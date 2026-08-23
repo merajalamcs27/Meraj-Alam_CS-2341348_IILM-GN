374882aa0fcc690e70f92075c5639c00b65bffec
Meraj Alam <meraj.alam.cs27@iilm.edu>
2026-08-24 03:08:30 +0530

    1657-determine-if-two-strings-are-close | 2026-08-24 03:08

---
+++ DSA/1657-determine-if-two-strings-are-close.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean closeStrings(String word1, String word2) {
+
+        int freq1[] = new int[26];
+        int freq2[] = new int[26];
+
+        for (char c : word1.toCharArray()) {
+            freq1[c - 'a']++;
+        }
+
+        for (char c : word2.toCharArray()) {
+            freq2[c - 'a']++;
+        }
+
+        // Both strings must contain the same characters
+        for (int i = 0; i < 26; i++) {
+        if (freq1[i] == 0 && freq2[i] != 0) {
+        return false;
+            }
+
+        if (freq1[i] != 0 && freq2[i] == 0) {
+        return false;
+            }
+        }
+
+        Arrays.sort(freq1);
+        Arrays.sort(freq2);
+
+        // Frequencies must be the same multiset
+        for (int i = 0; i < 26; i++) {
+            if (freq1[i] != freq2[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
