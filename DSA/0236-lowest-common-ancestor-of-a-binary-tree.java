dec9f34048e5e82bb6a7892b50d650107c4e7465
Meraj Alam <meraj.alam.cs27@iilm.edu>
2026-08-26 10:05:28 +0530

    0236-lowest-common-ancestor-of-a-binary-tree | 2026-08-26 10:05

--- DSA/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ DSA/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -9,11 +9,12 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null || root==p || root == q )return root;
+        if(root == null)return null;
+        if(root == p || root == q)return root;
         TreeNode left = lowestCommonAncestor(root.left,p,q);
         TreeNode right = lowestCommonAncestor(root.right,p,q);
         if(left == null)return right;
         if(right == null)return left;
-        return root; 
+        return root;
     }
 }
