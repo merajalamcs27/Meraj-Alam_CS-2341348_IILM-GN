5dea21b37a4a03a975c85021e73c925ffdc3b1fa
Meraj Alam <meraj.alam.cs27@iilm.edu>
2026-08-28 18:04:31 +0530

    0796-rotate-string | 2026-08-28 18:04

--- DSA/0796-rotate-string.java
+++ DSA/0796-rotate-string.java
@@ -1,10 +1,10 @@
 class Solution {// with using extra space
     public boolean rotateString(String s, String goal) {
         if(s.length()!=goal.length())return false;
-        String s1 = s+s;
-        for(int i = 0;i<s1.length()-goal.length();i++){
+        int n = s.length();
+        for(int i = 0;i<n;i++){
             int j =0;
-            while( j<goal.length() && s1.charAt(i+j) == goal.charAt(j)){
+            while( j<goal.length() && s.charAt((i+j)%n) == goal.charAt(j)){
                 j++;
             }
             if(j == goal.length())return true;
